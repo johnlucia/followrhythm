0eaaf7a60c7db5e3347c4115a4078cefea90d4e4
johnlucia <johnmlucia@gmail.com>
2018-10-30 20:47:51 -0700

    cloudinary in the admin views

--- app/decorators/models/refinery/image_decorator.rb
+++ app/decorators/models/refinery/image_decorator.rb
@@ -10,4 +10,12 @@ Refinery::Image.class_eval do
       url
     end
   end
+
+  def best_tiny_url
+    if external_url
+      external_url.gsub("q_auto,f_auto,fl_lossy", "w_100,h_100,c_fill,q_auto,f_auto,fl_lossy")
+    else
+      url
+    end
+  end
 end
